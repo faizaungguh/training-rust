0e424be646a6b812dc503bb0a43789c667cf1080
faizaungguh <otemoos@outlook.com>
2023-12-26 21:48:25 +0700

    mut untuk mutable variabel

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let pesan_satu: i32 = 1;
+    let  mut pesan_satu: i32 = 1;
     let pesan: &str = "hallo hai";
     println!("pesan ke {}: {}", pesan_satu, pesan);
 
-    pesan_satu = 2; // immutable variable akan membatasi perubahan ke variable yang telah berisi nilai sebelumnya, mirip dengan const
-    let pesan2: &str = "hallo hai";
+    pesan_satu = 2; // keyword mut membuat variabel bisa diubah nilainya
+    let pesan2: &str = "hallo mut";
     println!("pesan ke {}: {}", pesan_satu, pesan2);
 }
